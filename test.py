01e2843d048f3f2d37b84a43d494c58ae29d9e78
HoneyFox <git@honeyfox.uk>
2019-10-20 12:01:28 +0100

    rip random

--- test.py
+++ test.py
@@ -10,22 +10,20 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
+        self.btnCalculate = QtWidgets.QPushButton("Calculate!")
         self.text = QtWidgets.QLabel("Hello World")
         self.text.setAlignment(QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.btnCalculate)
         self.setLayout(self.layout)
 
-        self.button.clicked.connect(self.magic)
+        self.btnCalculate.clicked.connect(self.magic)
 
 
     def magic(self):
-        self.text.setText(random.choice(self.hello))
+        self.text.setText("E")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
