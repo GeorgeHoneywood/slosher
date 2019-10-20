c1539d24235d64030f38a9571aac544bfe399520
HoneyFox <git@honeyfox.uk>
2019-10-20 13:18:38 +0100

    surprised pikachu

--- slosher.py
+++ slosher.py
@@ -16,6 +16,7 @@ class MyWidget(QtWidgets.QWidget):
         self.btnCalculate = QtWidgets.QPushButton("Calculate!")
         self.txtHeight = QLineEdit(self)
         self.txtWeight = QLineEdit(self)
+        self.cbGender = QCheckBox("Male?", self)
         #self.txtGender = 
         #self.text.setAlignment(QtCore.Qt.AlignCenter)
 
@@ -25,9 +26,18 @@ class MyWidget(QtWidgets.QWidget):
     def layoutUI(self):
         self.layout = QtWidgets.QVBoxLayout()
 
+        
+        self.layout.addWidget(self.formGroupBox)
+
         self.layout.addWidget(self.txtHeight)
         self.layout.addWidget(self.btnCalculate)
+
         self.setLayout(self.layout)
+
+        # mainLayout = QVBoxLayout()
+        # mainLayout.addWidget(self.formGroupBox)
+        # mainLayout.addWidget(buttonBox)
+        # self.setLayout(mainLayout)
 
     def connectUI(self):
         self.btnCalculate.clicked.connect(self.calculate)
